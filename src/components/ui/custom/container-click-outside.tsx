076971fd1e6cde4084169a6f876173370887bcad
daniel-hortencio <hortencio.daniel@gmail.com>
2025-03-27 13:43:57 -0300

    swipe for menu mobile

--- src/components/ui/custom/container-click-outside.tsx
+++ src/components/ui/custom/container-click-outside.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 
 export type ClickOutsideProps = {
   element?: keyof HTMLElementTagNameMap;
@@ -23,12 +23,15 @@ export const ContainerClickOutside = ({
     }
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     document.addEventListener("click", handleClickOutside, true);
+    document.addEventListener("touchstart", handleClickOutside, true);
+
     return () => {
       document.removeEventListener("click", handleClickOutside, true);
+      document.removeEventListener("touchstart", handleClickOutside, true);
     };
-  }, [ref]);
+  }, []);
 
   return <div ref={ref} {...rest} />;
 };
