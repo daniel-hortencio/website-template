774029e9fabb34bb3fae6835a72de67b1fd2a82a
daniel-hortencio <hortencio.daniel@gmail.com>
2025-03-27 19:00:04 -0300

    portfolio

--- src/components/ui/custom/container-click-outside.tsx
+++ src/components/ui/custom/container-click-outside.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useMemo, useRef } from "react";
+import React, { useRef } from "react";
 
 export type ClickOutsideProps = {
   element?: keyof HTMLElementTagNameMap;
